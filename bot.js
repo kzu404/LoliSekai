31f6188137b539e81ca1332564d38d84d4ba72af
kzu404 <51977979+kzu404@users.noreply.github.com>
2019-06-19 12:54:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,28 @@ client.on('message', message => {
      if (message.content === 'hai') {
     	message.reply('hai juga kak');
   	}
+    if (message.content === 'price') {
+		 var coin = content.replace("$price ", "");
+		 var value = '';
+		 try{
+			request('http://api.coinmarketcap.com/v1/ticker/' + coin + '/',
+			function(error,res,body) {
+			var obj = JSON.parse(body);
+			console.log(obj[0]);
+			if(obj[0] === undefined)
+				{
+					message.reply('Wrong id');
+				}
+			else
+				{
+					value = coin.toUpperCase() +
+				" : Current Price " + obj[0].price_usd +
+				" | 24hr Percentage Change " + obj[0].percent_change_24h;
+				message.reply(value);
+				}
+				});
+				}
+  	}
 });
 
 
