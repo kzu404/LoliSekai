68c43953a172f25aa021e9749f30531d687a6187
kzu404 <51977979+kzu404@users.noreply.github.com>
2019-06-24 20:19:29 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,75 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 const request = require('request');
 
 client.on('ready', () => {
-    console.log('I am ready!');
-});
+    console.log("Connected as " + client.user.tag)
+})
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-     if (message.content === 'hai') {
-    	message.reply('hai juga kak');
-  	}
-    if (message.content === '.price ') {
-		 var coin = content.replace(".price ", "");
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Antisipasi bot membaca text sendiri
+        return
+    }
+    
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+})
+
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Menghilangkan Kata Perintah
+    let splitCommand = fullCommand.split(" ") // Memotong (split) kata setelah perintah tiap spasi
+    let primaryCommand = splitCommand[0] // Mengambil kata pertama setelah !
+	let arguments = splitCommand.slice(1) // Mengambil nilai setelah perintah
+	
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments)
+
+    if (primaryCommand == "help") {
+        helpCmd(arguments, receivedMessage)
+	}else if (primaryCommand == "lv") {
+        lvlCmd(arguments, receivedMessage)
+    } else if (primaryCommand == "ping") {
+        pingCmd(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("Perintah tidak di kenal silahkan ketik !help")
+    }
+}
+
+function helpCmd(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("Bantuan untuk perintah " + arguments)
+    } else {
+        receivedMessage.channel.send("Silahkan ketikan perintah !help nama_perintah agar lebih jelas.")
+    }
+}
+function lvlCmd(arguments, receivedMessage) {
+	 if (arguments.length > 0) {
+		 var coin = arguments;
 		 var value = '';
 		 try{
-			request('http://api.coinmarketcap.com/v1/ticker/' + coin + '/',
+			request('http://betech.id/api/lvlapi.php?lvl=' + coin,
 			function(error,res,body) {
-			var obj = JSON.parse(body);
-			message.reply(obj[0]);
-			if(obj[0] === undefined)
-				{
-					message.reply('Wrong id');
+				var jsonData = JSON.parse(body);
+				for (var i = 0; i < jsonData['data'].length; i++) {
+				var counter = jsonData['data'][i];
+				console.log(counter['monster']);
+				value = "Leveling " + coin +
+				": MOB Name: " + counter['monster'] + "[" + counter['level'] + "], Map: " + counter['lokasi'];
+				receivedMessage.channel.send(value);
+			}
 				}
-			else
-				{
-					value = coin.toUpperCase() +
-				" : Current Price " + obj[0].price_usd +
-				" | 24hr Percentage Change " + obj[0].percent_change_24h;
-				message.reply(value);
-				}
-				});
+				);
 				}
 				catch (err) {
-				message.reply('Wrong ID, Have a Great Day');
+				 console.log(err);
 				}
-  	}
-});
+	 }
+}
+function pingCmd(arguments, receivedMessage) {
 
+        receivedMessage.channel.send("Tutorial Bot Discord by https://betech.id")
+    
+}
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
